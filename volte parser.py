40b14acd5440baaf58f52633e0f1cdbe523c09f4
ileno <gustavo_antonio51@msn.com>
2019-06-26 10:09:41 -0300

    Test

--- volte parser.py
+++ volte parser.py
@@ -1,42 +1,37 @@
-s = open('E:\\Downloads\\MemExpFile_HSS9860_31.txt', 'r')
-s = s.readlines()
-s = str(s[1:])
-s = s.strip()
 import re
 import pandas as pd
+import sys
 
-
-
-regex1 = r'IMPI=(?P<IMPI>)[+0-9]+[^\d][a-z]+[^\d][a-z]+[0-9]+[^\d][a-z]+[0-9]+[^\d][3][a-z]+[^\d][a-z]+'
+# Regex matches one IMPI with next UserState - any IMPI in between is skipped
+regex =  r"IMPI=(?P<IMPI>)[+0-9]+[^\d][a-z]+[^\d][a-z]+[0-9]+[^\d][a-z]+[0-9]+[^\d][3][a-z]+[^\d][a-z]+"
 regex2 = r"UserState=(?P<UserState>[1-2]+)"
-regex3 = r'[^\d](SUBBEGIN)'
-regex4 = r'[^\d](SUBEND)'
 data_dict = {}
-def ordered_set(inlist):
-    out_list = []
-    for val in inlist:
-        if not val in out_list:
-            out_list.append(val)
-    return out_list
+def parse_data(infile):
+    with open(infile, 'r') as s:
+        s = s.readlines()
+        s = str(s[1:])
+        data = []
+        m = re.findall('SUBBEGIN(.*?)SUBEND', s, re.MULTILINE)
+        for e in m:
+            impi = re.search(regex, e).group().split('=')
+            data.append(impi)
+            if e.find('UserState') > -1:
+                user = re.search(regex2, e)
+                if user:
+                    state = user.group().split('=')
+                    data.append(state)
+            else:
+                data.append(['UserState', '0'])
+    return data
 
-def str_match(s):
-    data = []
-    for match in re.finditer(regex1, s, re.MULTILINE|re.DOTALL):
-        matches1 = match.group()
-        print(matches1)
-        list_impu = matches1.split('=')
-    for match in re.finditer(regex2, s, re.MULTILINE|re.DOTALL):
-        data.append(ordered_set(list_impu))
-        matches2 = match.group()
-        data.append(matches2.split('='))
-    return data 
 
+if __name__ == "__main__":
+    infile = sys.argv[1]
+    for i in parse_data(infile):
+        if i[0] not in data_dict:
+            data_dict[i[0]] = [i[1]]
 
-for i in str_match(s):
-    if i[0] not in data_dict:
-        data_dict[i[0]] = [i[1]]
+        else:
+            data_dict[i[0]].append(i[1])
 
-    else:
-        data_dict[i[0]].append(i[1])
-
-pd.DataFrame.from_dict(data_dict, orient = 'index').T.to_csv('filename.csv')
+    pd.DataFrame.from_dict(data_dict, orient = 'index').T.to_csv(sys.argv[2])
